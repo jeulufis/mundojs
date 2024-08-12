fccfd5d4480ef09a2c04009db7f7f8700bf7bd49
jeulufis <juan.eulufi.sa@gmail.com>
2024-08-11 21:29:33 -0400

    nuevo blog 03

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
       {allPosts.map((post) => (
         <article key={post._id}>
           <Link href={post.slug}>
-            <h2>{post.title}</h2>
+            <h2 className="dark:text-yellow-400">{post.title}</h2>
           </Link>
           {post.description && <p>{post.description}</p>}
         </article>
