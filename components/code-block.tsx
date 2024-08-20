5fa7177cab50f694ae4b5eeb36600e286b27a55b
jeulufis <juan.eulufi.sa@gmail.com>
2024-08-20 16:39:12 -0400

    refactor: Remove unused code and update CodeBlock component for syntax highlighting

--- components/code-block.tsx
+++ components/code-block.tsx
@@ -1,16 +1,14 @@
 "use client";
 
-import { useTheme } from "next-themes";
 import SyntaxHighlighter from "react-syntax-highlighter";
-import { stackoverflowLight, stackoverflowDark } from "react-syntax-highlighter/dist/esm/styles/hljs";
+import { stackoverflowDark } from "react-syntax-highlighter/dist/esm/styles/hljs";
 
 
 
 const CodeBlock = ({ children }: any) => {
-  const { theme } = useTheme();
 
   return (
-    <SyntaxHighlighter language="javascript"  style={theme === 'dark' ? stackoverflowDark : stackoverflowLight}>
+    <SyntaxHighlighter language="javascript"  style={stackoverflowDark}>
       {children}
     </SyntaxHighlighter>
 
