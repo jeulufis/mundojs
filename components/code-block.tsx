23f6c2bd30226dd9c7092bdee2f24898255bdb58
jeulufis <juan.eulufi.sa@gmail.com>
2024-08-20 16:31:52 -0400

    refactor: Update CodeBlock component to use dynamic theme for syntax highlighting

--- components/code-block.tsx
+++ components/code-block.tsx
@@ -1,13 +1,19 @@
 "use client";
 
+import { useTheme } from "next-themes";
 import SyntaxHighlighter from "react-syntax-highlighter";
-import { atomOneDarkReasonable } from "react-syntax-highlighter/dist/esm/styles/hljs";
+import { stackoverflowLight, stackoverflowDark } from "react-syntax-highlighter/dist/esm/styles/hljs";
+
+
 
 const CodeBlock = ({ children }: any) => {
+  const { theme } = useTheme();
+
   return (
-    <SyntaxHighlighter language="javascript" style={atomOneDarkReasonable}>
+    <SyntaxHighlighter language="javascript"  style={theme === 'dark' ? stackoverflowDark : stackoverflowLight}>
       {children}
     </SyntaxHighlighter>
+
   );
 };
 
